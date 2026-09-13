0f867a7581d177e584c1e8d539b8f52c059ffe9e
estefanno monar <estefannomonarsoto2004@gmail.com>
2026-09-13 18:42:28 -0300

    cambios

--- src/main/java/campuslab/ms_campuslab_bookings/dto/StatusUpdateRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package campuslab.ms_campuslab_bookings.dto;
-
-import campuslab.ms_campuslab_bookings.model.BookingStatus;
-import jakarta.validation.constraints.NotNull;
-
-public record StatusUpdateRequestDTO(
-        @NotNull BookingStatus status,
-        String technicianId
-) {}
--- src/main/java/campuslab/ms_campuslab_bookings/exception/InvalidStatusTransitionException.java
+++
@@ -1,7 +0,0 @@
-package campuslab.ms_campuslab_bookings.exception;
-
-public class InvalidStatusTransitionException extends RuntimeException {
-    public InvalidStatusTransitionException(String message) {
-        super(message);
-    }
-}
